46ee1a062b069c91a1970178c5cc2589f47bf04c
Mehmet Celik <mehmet@celik.be>
2011-03-30 08:44:49 +0200

    added ability to show Primo Central records

--- show_pnx.js
+++ show_pnx.js
@@ -5,10 +5,10 @@
  * This plugin uses the Primo Web Services to return data please make sure
  * the are not blocked or restricted by a Firewall or by Configuration
  *
- * Version: 0.2
+ * Version: 0.1
  *
- * Libis (c) 2010 -- BSD license
- * Mehmet Celik -- mehmet.celik at libis.be
+ * Libis (c) 2010
+ * Mehmet Celik
  * 
  */
 var pnx_record = null;
@@ -21,14 +21,25 @@ jQuery(document).ready(function() {
     $('.EXLResultRecordId').each(
     function(index, element) {
         var record_id = $(this).attr('id');
-	if ((record_id.substring(0, 2) != 'TN') && (record_id.substring(0, 5) != 'dedup')) { //skip PrimoCentral records and dedup records
+
+        if ((record_id.substring(0, 5) != 'dedup')) { //skip dedup records
+        //if ((record_id.substring(0, 2) != 'TN') && (record_id.substring(0, 5) != 'dedup')) { //skip PrimoCentral records and dedup records
             $(this.parentNode).append("<a class='showPNX' data-pnx='" + record_id + "' style='font-size:0.5em;'>Show PNX</a>");
             $('.showPNX').live('click',
             function() {
                 var rec_id = $(this).attr('data-pnx');
+                var search = "local";
+                //search local or remote
+                if ((rec_id.substring(0, 2) == 'TN') ) {
+                  search = "adaptor,primo_central_multiple_fe";
+                  rec_id = rec_id.substring(3);
+                }
+
+
                 var q = 'any,contains,' + rec_id;
                 var xmlpnx = $.ajax({
                     url: '/PrimoWebServices/xservice/search/brief',
+		    dataType: 'xml',
                     data: {
                         institution: 'KUL',
                         query: q,
@@ -37,7 +48,8 @@ jQuery(document).ready(function() {
                         bulkSize: 1,
                         dym: 'false',
                         highlight: 'false',
-                        lang: 'eng'
+                        lang: 'eng',
+                        loc: search
                     },
                     async: false,
                     error: function(request, status, error) {
@@ -45,16 +57,25 @@ jQuery(document).ready(function() {
                         alert('Ooops: ' + request.statusText + ' --> ' + request.responseText);
                     }
                 }).responseXML;
-                pnx_record = $(xmlpnx).find('record').eq(0);
                 var strpnx = "";
-                if (window.ActiveXObject) {
-                    strpnx = pnx_record[0].xml;
-                }
-                else {
-                    strpnx = (new XMLSerializer()).serializeToString(pnx_record[0]);
-                }
+                pnx_record = $(xmlpnx).find('record').eq(0);
+//PRIMO CENTRAL records have a namespace
+		if (pnx_record.size() == 0){
+		    pnx_record = $(xmlpnx).find('prim\\:record').eq(0);	
+		}
 
-                strpnx = strpnx.replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+                if (pnx_record.size() > 0) {
+                  if (window.ActiveXObject) {
+                      strpnx = pnx_record[0].xml;
+                  }
+                  else {
+                      strpnx = (new XMLSerializer()).serializeToString(pnx_record[0]);
+                  }
+
+                  strpnx = strpnx.replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+                } else {
+                  strpnx = "No data loaded";
+                }
                 $('#PNXData_body').empty().append("<pre>" + strpnx + "</pre>");
                 $('#PNXData').show();
                 scrollTo(0, 0);
