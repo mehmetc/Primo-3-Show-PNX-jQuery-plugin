fe3bb3df91a22ac1a480024e048e96c06f4ff28d
Mehmet Celik <mehmet@celik.be>
2012-09-13 14:25:24 +0200

    Latest jQuery changed selector syntax

--- show_pnx.js
+++ show_pnx.js
@@ -7,7 +7,7 @@
  *
  * !!!!!!!!!!!! update the 'defaultInstitutioncode' variable !!!!!!!!!!!!!!!
  *
- * Version: 0.2
+ * Version: 0.3
  *
  * Libis (c) 2010
  * Mehmet Celik
@@ -55,9 +55,12 @@ function loadPNXRecord(recordId, institutionCode) {
     
     pnxRecord = $(xmlpnx).find('record').eq(0);
 
-    //PRIMO CENTRAL records have a namespace prefix. Local PNX record don't
-    if (pnxRecord.size() == 0) {
-        pnxRecord = $(xmlpnx).find('[nodeName="prim:record"]').eq(0);
+    //PRIMO CENTRAL records have a namespace prefix. Local PNX records don't
+    if (pnx_record.size() == 0){
+        pnx_record = $(xmlpnx).find('[nodeName="prim:record"]').eq(0);      
+        if(pnx_record.size() == 0) { //some jQuery versions refuse the above query
+           pnx_record = $(xmlpnx).find('prim\\:record').eq(0);      
+        }
     }
 
     if (pnxRecord.size() > 0) {
